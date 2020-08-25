1b8529c50bf08d8a0136d7319393e4263e6a8936
Willem A Duiker <duikhead2@gmail.com>
2020-08-25 18:20:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,13 @@
 # Code your solution here!
+
+def run_guessing_game
+  number = rand(1..6)
+  puts "Guess a number between 1-6"
+  input = gets.chomp
+  if input == number
+    puts "Good job!"
+  else
+    puts "Guess again!"
+  end
+end
+  
